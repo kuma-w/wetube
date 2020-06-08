8536b1d8e0f06e062e4f2ca1660c508746638ef5
dev-woong <dev-woong>
2020-06-09 01:01:13 +0900

    babel commit

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-console.log("Hi!");
+import express from "express";
+const app = express()
+
+const port = 4000;
+const handleListening = () => console.log('Listening on: http://localhost:${port}');
+
+const handleHome = (req, res) => res.send('Hello from home!');
+
+
+const handleProfile = (req, res) => res.send("You are on my profile");
+
+app.get("/",handleHome);
+
+app.get("/profile", handleProfile);
+
+app.listen(port, handleListening);
